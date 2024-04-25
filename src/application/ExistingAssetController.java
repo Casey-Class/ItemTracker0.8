2c71659e0cbf0eb9d0870c22b2d75925a5784cc6
Casey Class <caseyclass71@gmail.com>
2024-04-24 21:14:17 -0700

    controller connected

--- src/application/ExistingAssetController.java
+++ src/application/ExistingAssetController.java
@@ -1,12 +1,26 @@
 package application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExistingAssetController {
 
@@ -46,4 +60,71 @@ public class ExistingAssetController {
     @FXML
     private TableColumn<?, ?> LocationCol;
 
+    @FXML
+    void goHome(ActionEvent event) {
+
+    }
+
+    private List<String> assets = new ArrayList<>(); // Array that stores assets
+    private ObservableList<Asset> assetObservableList = FXCollections.observableArrayList();
+
+    public void initialize() {
+        // Set initial text for category display label
+        PageLabel.setText("Asset List");
+        AssetNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        CategoryCol.setCellValueFactory(new PropertyValueFactory<>("category"));
+        LocationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+        ViewItemCol.setCellValueFactory(new PropertyValueFactory<>("viewItem"));
+        loadAssets(); // load entries from assets.csv into the table
+    }
+
+    public void loadAssets() {
+        String filePath = "assets.csv";
+
+        if (Files.exists(Paths.get(filePath))) {
+            try {
+                List<String> lines = Files.readAllLines(Paths.get(filePath));
+
+                // Clear existing assets List
+                assets.clear();
+
+                // add each asset to assets
+                for (String line : lines) {
+                    String[] parts = line.split(":");
+                    assetObservableList.add(new Asset(parts[0], parts[1], parts[2]));
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading from CSV file: " + e.getMessage());
+            }
+        } else {
+            System.out.println("File does not exist: " + filePath);
+        }
+    }
+
+    /**
+
+     Handles the action event when the "Back To Home" button is clicked.
+     Loads the homepage FXML file and sets it as the scene for the stage.
+     */
+    public void goHome() {
+        try {// Load the FXML file for the welcome page
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Welcome.fxml"));
+            Parent root = loader.load();
+
+            // Get the stage from the current scene
+            Stage stage = (Stage) PageLabel.getScene().getWindow();
+
+            // Set the new scene for the stage
+            Scene scene = new Scene(root);
+
+            // Set the title of the stage
+            stage.setTitle("Welcome to TrackWise");
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception appropriately
+        }
+    }
 }
